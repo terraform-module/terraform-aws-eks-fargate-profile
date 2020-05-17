ee8201cb8b3e32d612f8be7017f5900a263be1ac
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2020-05-17 17:31:23 +0100

    bug in suffix

--- main.tf
+++ main.tf
@@ -19,10 +19,11 @@ data aws_iam_policy_document assume_role {
 
 resource aws_iam_role this {
   count              = var.enabled ? 1 : 0
-  name               = format("%s-%s", local.name, local.suffix)
+  name               = format("%s%s", local.name, local.suffix)
   assume_role_policy = join("", data.aws_iam_policy_document.assume_role.*.json)
   tags = merge(var.tags,
     { Namespace = var.namespace },
+    { "kubernetes.io/cluster/${var.cluster_name}" = "owned" },
   { "k8s.io/cluster/${var.cluster_name}" = "owned" })
 }
 
@@ -35,9 +36,10 @@ resource aws_iam_role_policy_attachment attachment_main {
 resource aws_eks_fargate_profile this {
   count                  = var.enabled ? 1 : 0
   cluster_name           = var.cluster_name
-  fargate_profile_name   = format("%s-%s", local.name, local.suffix)
+  fargate_profile_name   = format("%s%s", local.name, local.suffix)
   pod_execution_role_arn = join("", aws_iam_role.this.*.arn)
   subnet_ids             = var.subnet_ids
+
   tags = merge(var.tags,
     { Namespace = var.namespace },
     { "kubernetes.io/cluster/${var.cluster_name}" = "owned" },
