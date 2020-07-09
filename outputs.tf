a2def684a0d023af6fe3d1a3e63877ce03eca917
Jason Williams <jasonw@jhu.edu>
2020-07-09 14:45:59 -0400

    fix for outputs, they are sets now because of for_each

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 
 output "eks_fargate_profile_role_arn" {
   description = "ARN of the EKS Fargate Profile IAM role"
-  value       = join(",", aws_iam_role.this.*.arn)
+  value       = [for entry in aws_iam_role.this : entry.arn]
 }
 
 output "eks_fargate_profile_role_name" {
   description = "Name of the EKS Fargate Profile IAM role"
-  value       = join(",", aws_iam_role.this.*.name)
+  value       = [for entry in aws_iam_role.this : entry.name]
 }
 
 output "eks_fargate_profile_id" {
   description = "EKS Cluster name and EKS Fargate Profile name separated by a colon"
-  value       = join(",", aws_eks_fargate_profile.this.*.id)
+  value       = [for entry in aws_eks_fargate_profile.this : entry.id]
 }
 
 output "eks_fargate_profile_arn" {
   description = "Amazon Resource Name (ARN) of the EKS Fargate Profile"
-  value       = join(",", aws_eks_fargate_profile.this.*.arn)
+  value       = [for entry in aws_eks_fargate_profile.this : entry.arn]
 }
 
 output "eks_fargate_profile_status" {
   description = "Status of the EKS Fargate Profile"
-  value       = join(",", aws_eks_fargate_profile.this.*.status)
+  value       = [for entry in aws_eks_fargate_profile.this : entry.status]
 }
